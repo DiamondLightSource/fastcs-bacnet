4766a181b37c7b5ce62bc3cec09408dba7ce43a5
Oli Jones <oliver.jones@diamond.ac.uk>
2026-03-19 09:36:03 +0000

    First FastCS example written

--- src/fastcs_bacnet/__main__.py
+++ src/fastcs_bacnet/__main__.py
@@ -2,64 +2,14 @@
 
 from argparse import ArgumentParser
 from collections.abc import Sequence
-from dataclasses import dataclass
 
-from fastcs.attributes import AttributeIO, AttributeIORef, AttrR
-from fastcs.controllers import Controller
-from fastcs.datatypes import Float
-from fastcs.launch import FastCS
-from fastcs.transports.epics import EpicsIOCOptions
-from fastcs.transports.epics.ca import EpicsCATransport
+from fastcs_bacnet.examples.FastCS.CA_for_dummy_device import (
+    single_controller_multi_constants,
+)
 
 from . import __version__
 
 __all__ = ["main"]
-
-
-class DeviceObject:
-    def __init__(self, number_of_fields: int):
-        self.number_of_fields = number_of_fields
-        self.store: dict[int, float] = {}
-
-        for i in range(number_of_fields):
-            self.store[i] = (i * 439 % 238) / 57
-
-    def get(self, index: int):
-        return self.store[index]
-
-
-@dataclass
-class IndexAttributeIORef(AttributeIORef):
-    index: int
-
-
-class IndexAttributeIO(AttributeIO[float, IndexAttributeIORef]):
-    def __init__(self, device: DeviceObject):
-        super().__init__()
-
-        self.device = device
-
-    async def update(self, attr: AttrR[float, IndexAttributeIORef]):
-
-        await attr.update(self.device.get(attr.io_ref.index))
-
-
-class SimpleController(Controller):
-    def __init__(self, device: DeviceObject):
-        super().__init__(ios=[IndexAttributeIO(device)])
-
-        for i in range(device.number_of_fields):
-            self.add_attribute(
-                "attribute_" + str(i),
-                AttrR(Float(), io_ref=IndexAttributeIORef(i, update_period=1)),
-            )
-
-
-device_object = DeviceObject(1500)
-
-epics_ca = EpicsCATransport(epicsca=EpicsIOCOptions(pv_prefix="DEMO"))
-local_controller = SimpleController(device_object)
-fastcs = FastCS(local_controller, [epics_ca])
 
 
 def main(args: Sequence[str] | None = None) -> None:
@@ -74,12 +24,7 @@ def main(args: Sequence[str] | None = None) -> None:
     parser.parse_args(args)
 
     print("hello world!!")
-    fastcs.run()
-
-    # TO TEST:
-    # 1: run this file (fastcs-bacnet)
-    # 2: start terminal (outside of container)
-    # 3: run command "caget DEMO:Attribute943"
+    single_controller_multi_constants()
 
 
 if __name__ == "__main__":
---
+++ src/fastcs_bacnet/examples/FastCS/CA_for_dummy_device.py
@@ -0,0 +1,20 @@
+from fastcs.controllers.controller import Controller
+from fastcs.launch import FastCS
+from fastcs.transports.epics import EpicsIOCOptions
+from fastcs.transports.epics.ca import EpicsCATransport
+
+from fastcs_bacnet.dummy_device import DummyDevice
+from fastcs_bacnet.dummy_device_controller import DummyDeviceController
+
+
+def start_fastcs_ca_with_controller(controller: Controller):
+    epics_ca = EpicsCATransport(epicsca=EpicsIOCOptions(pv_prefix="DEMO"))
+    fastcs = FastCS(controller, [epics_ca])
+    fastcs.run()
+
+
+def single_controller_multi_constants():
+    dummy_device = DummyDevice(number_of_constant_fields=1500)
+    dummy_controller = DummyDeviceController(dummy_device)
+
+    start_fastcs_ca_with_controller(dummy_controller)
