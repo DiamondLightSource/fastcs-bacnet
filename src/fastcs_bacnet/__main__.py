a98d014e7fb47a6c08254adcbb4f640001e655ae
Oli Jones <oliver.jones@diamond.ac.uk>
2026-03-19 09:33:28 +0000

    Basic FastCS demo put in main

--- src/fastcs_bacnet/__main__.py
+++ src/fastcs_bacnet/__main__.py
@@ -2,40 +2,64 @@
 
 from argparse import ArgumentParser
 from collections.abc import Sequence
-from pathlib import Path
+from dataclasses import dataclass
 
-from fastcs import FastCS
-from fastcs.attributes import AttrR
-from fastcs.connections import IPConnection, IPConnectionSettings
+from fastcs.attributes import AttributeIO, AttributeIORef, AttrR
 from fastcs.controllers import Controller
-from fastcs.datatypes import String
-from fastcs.transports.epics import EpicsGUIOptions, EpicsIOCOptions
-from fastcs.transports.epics.ca.transport import EpicsCATransport
+from fastcs.datatypes import Float
+from fastcs.launch import FastCS
+from fastcs.transports.epics import EpicsIOCOptions
+from fastcs.transports.epics.ca import EpicsCATransport
 
 from . import __version__
 
 __all__ = ["main"]
 
 
-class MyController(Controller):
-    device_id = AttrR(String())
+class DeviceObject:
+    def __init__(self, number_of_fields: int):
+        self.number_of_fields = number_of_fields
+        self.store: dict[int, float] = {}
 
-    def __init__(self, settings: IPConnectionSettings):
+        for i in range(number_of_fields):
+            self.store[i] = (i * 439 % 238) / 57
+
+    def get(self, index: int):
+        return self.store[index]
+
+
+@dataclass
+class IndexAttributeIORef(AttributeIORef):
+    index: int
+
+
+class IndexAttributeIO(AttributeIO[float, IndexAttributeIORef]):
+    def __init__(self, device: DeviceObject):
         super().__init__()
 
-        self._ip_settings = settings
-        self._connection = IPConnection()
+        self.device = device
 
-    async def connect(self):
-        await self._connection.connect(self._ip_settings)
+    async def update(self, attr: AttrR[float, IndexAttributeIORef]):
+
+        await attr.update(self.device.get(attr.io_ref.index))
 
 
-gui_options = EpicsGUIOptions(
-    output_path=Path(".") / "demo.bob", title="Demo Temperature Controller"
-)
-epics_ca = EpicsCATransport(gui=gui_options, epicsca=EpicsIOCOptions(pv_prefix="DEMO"))
-connection_settings = IPConnectionSettings("localhost", 25565)
-fastcs = FastCS(MyController(connection_settings), [epics_ca])
+class SimpleController(Controller):
+    def __init__(self, device: DeviceObject):
+        super().__init__(ios=[IndexAttributeIO(device)])
+
+        for i in range(device.number_of_fields):
+            self.add_attribute(
+                "attribute_" + str(i),
+                AttrR(Float(), io_ref=IndexAttributeIORef(i, update_period=1)),
+            )
+
+
+device_object = DeviceObject(1500)
+
+epics_ca = EpicsCATransport(epicsca=EpicsIOCOptions(pv_prefix="DEMO"))
+local_controller = SimpleController(device_object)
+fastcs = FastCS(local_controller, [epics_ca])
 
 
 def main(args: Sequence[str] | None = None) -> None:
@@ -48,8 +72,14 @@ def main(args: Sequence[str] | None = None) -> None:
         version=__version__,
     )
     parser.parse_args(args)
+
     print("hello world!!")
     fastcs.run()
+
+    # TO TEST:
+    # 1: run this file (fastcs-bacnet)
+    # 2: start terminal (outside of container)
+    # 3: run command "caget DEMO:Attribute943"
 
 
 if __name__ == "__main__":
