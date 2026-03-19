29aec857edeb5149489b4d7d92446330b4420bd2
Oli Jones <oliver.jones@diamond.ac.uk>
2026-03-19 10:28:41 +0000

    Fixed full cbLow ring buffer bug

--- src/fastcs_bacnet/examples/FastCS/CA_for_dummy_device.py
+++ src/fastcs_bacnet/examples/FastCS/CA_for_dummy_device.py
@@ -2,6 +2,7 @@ from fastcs.controllers.controller import Controller
 from fastcs.launch import FastCS
 from fastcs.transports.epics import EpicsIOCOptions
 from fastcs.transports.epics.ca import EpicsCATransport
+from softioc.imports import callbackSetQueueSize
 
 from fastcs_bacnet.dummy_device import DummyDevice
 from fastcs_bacnet.dummy_device_controller import DummyDeviceController
@@ -10,11 +11,13 @@ from fastcs_bacnet.dummy_device_controller import DummyDeviceController
 def start_fastcs_ca_with_controller(controller: Controller):
     epics_ca = EpicsCATransport(epicsca=EpicsIOCOptions(pv_prefix="DEMO"))
     fastcs = FastCS(controller, [epics_ca])
+    # increases the number of attributes allowed
+    callbackSetQueueSize(16000)
     fastcs.run()
 
 
 def single_controller_multi_constants():
-    dummy_device = DummyDevice(number_of_constant_fields=1500)
+    dummy_device = DummyDevice(number_of_constant_fields=15000)
     dummy_controller = DummyDeviceController(dummy_device)
 
     start_fastcs_ca_with_controller(dummy_controller)
