5ae4b4382a453cfdae1095ad49ff2dc34a0bcf31
Oli Jones <oliver.jones@diamond.ac.uk>
2026-03-25 16:22:59 +0000

    device variable initialisation written

--- src/fastcs_bacnet/dummy/generic/device.py
+++ src/fastcs_bacnet/dummy/generic/device.py
@@ -1,4 +1,16 @@
+import random
+
+from fastcs_bacnet.dummy.generic.device_variables.constant_variable import (
+    ConstantVariable,
+)
 from fastcs_bacnet.dummy.generic.device_variables.device_variable import DeviceVariable
+from fastcs_bacnet.dummy.generic.device_variables.oscillating_variable import (
+    OscillatingVariable,
+)
+from fastcs_bacnet.dummy.generic.device_variables.random_variable import RandomVariable
+from fastcs_bacnet.dummy.generic.device_variables.read_write_variable import (
+    ReadWriteVariable,
+)
 
 
 class Device:
@@ -12,4 +24,33 @@ class Device:
     ):
         self.variables: list[DeviceVariable] = []
 
-        pass
+        for i in range(number_of_constant_fields):
+            self.variables.append(
+                ConstantVariable(f"{device_name}_constant_{i}", random.random())
+            )
+
+        for i in range(number_of_oscillating_fields):
+            self.variables.append(
+                OscillatingVariable(
+                    f"{device_name}_oscillating_{i}",
+                    random.random(),
+                    random.random(),
+                    random.random() + 0.5,
+                )
+            )
+
+        for i in range(number_of_random_fields):
+            self.variables.append(
+                RandomVariable(
+                    f"{device_name}_random_{i}",
+                    random.random(),
+                    random.random() + 1,
+                    random.random(),
+                    random.random() + 1,
+                )
+            )
+
+        for i in range(number_of_read_write_fields):
+            self.variables.append(
+                ReadWriteVariable(f"{device_name}_oscillating_{i}", random.random())
+            )
