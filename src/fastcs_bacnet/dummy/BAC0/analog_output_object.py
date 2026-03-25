651f7e09a5a6414e47985c47576fb3da0f6cebda
Oli Jones <oliver.jones@diamond.ac.uk>
2026-03-25 16:54:50 +0000

    docstrings added to analog_output_object

--- src/fastcs_bacnet/dummy/BAC0/analog_output_object.py
+++ src/fastcs_bacnet/dummy/BAC0/analog_output_object.py
@@ -5,6 +5,11 @@ from fastcs_bacnet.dummy.generic.device_variables.device_variable import DeviceV
 
 
 class AnalogOutputObject:
+    """
+    Simulates a bacnet device object
+    Takes a DeviceVariable and mimics its values
+    """
+
     def __init__(
         self,
         device: BAC0.lite,
@@ -13,6 +18,20 @@ class AnalogOutputObject:
         instance: int,
         device_variable: DeviceVariable,
     ):
+        """
+        device: The device the object is being attached to
+        name: name of the object
+        description: description of the object
+        instance: instance number of this object on this device
+        device_variable: the DeviceVariable this object will mimic
+
+        Instance numbers must be unique per object type per device
+        This is the devices responsibility to handle
+
+        The device_variable's callback will be replaced
+        This is how the object mimics its value
+        """
+
         self.device = device
         self.name = name
         self.device_variable = device_variable
@@ -24,4 +43,10 @@ class AnalogOutputObject:
         device_variable.set_update_callback(self._update_value)
 
     def _update_value(self, new_value: float):
+        """
+        Callback function to update this objects value
+        """
+        # Ruff doesnt like this
+        # It is the way they do it in the docs
+        # Sometime Ruff is fine with it??
         self.device[self.name].presentValue = new_value  # type: ignore
