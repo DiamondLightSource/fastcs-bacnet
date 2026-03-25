8a242b2def32442f6e0cf125059e3be600ad305b
Oli Jones <oliver.jones@diamond.ac.uk>
2026-03-25 15:45:33 +0000

    docstrings written for device_variable

--- src/fastcs_bacnet/dummy/generic/device_variables/device_variable.py
+++ src/fastcs_bacnet/dummy/generic/device_variables/device_variable.py
@@ -3,16 +3,36 @@ from collections.abc import Callable
 
 
 class DeviceVariable(ABC):
+    """
+    Abstract class for a variable belonging to a device
+    This is intended to represent any of
+        A bacnet device Object
+        An EPICS PV
+        A FastCS Attribute
+    """
+
     _value: float | None
 
     def __init__(
         self, name: str, update_callback: Callable[[float], None] | None = None
     ):
+        """
+        name: name of variable, should be unique per device
+        update_callback: procedure that runs when variable value is updated
+        """
         self.name = name
         self.update_callback = update_callback
 
     def get_value(self) -> float | None:
+        """
+        Returns value of variable
+        """
         return self._value
 
     def set_update_callback(self, update_callback: Callable[[float], None] | None):
+        """
+        Changes the callback procedure of this variable
+        Use argument None to remove the current callback
+        This will affect updates immediately after the method is called
+        """
         self.update_callback = update_callback
