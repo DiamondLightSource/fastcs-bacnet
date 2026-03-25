cbc23b253aef2a1513d341d6225df7fcf2f50cf7
Oli Jones <oliver.jones@diamond.ac.uk>
2026-03-25 15:36:01 +0000

    random update loop implemented

--- src/fastcs_bacnet/dummy/generic/device_variables/random_variable.py
+++ src/fastcs_bacnet/dummy/generic/device_variables/random_variable.py
@@ -1,4 +1,5 @@
 import asyncio
+import random
 from collections.abc import Callable
 
 from fastcs_bacnet.dummy.generic.device_variables.device_variable import DeviceVariable
@@ -24,7 +25,17 @@ class RandomVariable(DeviceVariable):
         asyncio.create_task(self.start_update_loop())
 
     async def start_update_loop(self):
-        pass
+        while True:
+            self.update()
+            change_time = self.min_change_time + (
+                random.random() * (self.max_change_time - self.min_change_time)
+            )
+            await asyncio.sleep(change_time)
 
     def update(self):
-        pass
+        self._value = self.min_value + (
+            random.random() * (self.max_value - self.min_value)
+        )
+
+        if self.update_callback is not None:
+            self.update_callback(self._value)
