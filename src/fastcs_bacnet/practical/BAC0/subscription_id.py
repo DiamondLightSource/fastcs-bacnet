c63cbb8ddb4073d6152c568bfaddee83c239e60e
Oli Jones <oliver.jones@diamond.ac.uk>
2026-03-26 09:47:02 +0000

    docstring added for subscription id dataclass

--- src/fastcs_bacnet/practical/BAC0/subscription_id.py
+++ src/fastcs_bacnet/practical/BAC0/subscription_id.py
@@ -3,6 +3,15 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class SubscriptionID:
+    """
+    dataclass for identifying a specific bacnet object to subscribe to
+    address: ip address of the device that owns the object
+        in string format (e.g. "127.0.0.1")
+    port: port device is using for bacnet communication (e.g. 47808)
+    object_type: type of object belonging to the bacnet device (e.g. "analog-output")
+    object_id: instance number for this object on the device (e.g. 0)
+    """
+
     address: str
     port: int
     object_type: str
