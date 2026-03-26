a0cf1ac2a7394d5e9d6226a59eb87720737ecbb2
Oli Jones <oliver.jones@diamond.ac.uk>
2026-03-26 11:51:02 +0000

    docstrings added to bacnet_client

--- src/fastcs_bacnet/practical/BAC0/bacnet_client.py
+++ src/fastcs_bacnet/practical/BAC0/bacnet_client.py
@@ -7,6 +7,11 @@ from fastcs_bacnet.practical.BAC0.subscription_id import SubscriptionID
 
 
 class BacnetClient:
+    """
+    Creates and stores subscription objects to bacnet objects
+    Does NOT handle them
+    """
+
     def __init__(
         self,
         bacnet_client: lite | None = None,
@@ -15,6 +20,22 @@ class BacnetClient:
         default_generic_callback: Callable[[SubscriptionID, str, float], None]
         | None = None,
     ):
+        """
+        bacnet_client: python bacnet object used to interact with actual bacnet objects
+            if set to None one is created inside the object
+            if one is created remember to disconnect it using the disconnect method
+        initial_subscriptions: A list of SubsciptionIDs the object can use
+            to make subscriptions in the constructor
+            Just loops through this list and calls add_subscription
+        subscription_lifetime: Time that subscriptions last (in seconds)
+            subscriptions are auto renewed so this doesnt matter too much
+        default_generic_callback: Callbacks can be added to subscriptions
+            (procedures that run when a new value is recieved from the device)
+            This is a generic callback that can apply to any subscription and takes
+            its SubscriptionID as a parameter
+            This will be used as a defualt for added subscriptions
+            If None nothing happens when a new value is recieved
+        """
         self.subscription_lifetime = subscription_lifetime
         self.default_generic_callback = default_generic_callback
 
@@ -38,6 +59,12 @@ class BacnetClient:
         subscription_id: SubscriptionID,
         callback: Callable[[str, float]] | None = None,
     ):
+        """
+        Adds a new subscription object to the dictionary
+        subscription_id: identifier used to find the object to subscribe to
+        callback: Procedure that is called when a new value is recieved from the device
+            If None the default_generic_callback will be used
+        """
         # cant do the check for default_generic_callback being none here
         # because it could change
         if callback is None:
@@ -60,6 +87,13 @@ class BacnetClient:
     def remove_subscription(
         self, subscription_id: SubscriptionID, stop_subscription=True
     ):
+        """
+        Removes a subscription from the dictionary
+        subscription_id: identifier used to find the object to subscribe to
+        stop_subscription: if True, the subsciption itself is also stopped
+            Set to False if you have taken your own instance of the
+            ObjectSubscription that you are still using
+        """
         subscription = self.get_subscription(subscription_id)
         self.subscriptions.pop(subscription_id)
 
@@ -67,10 +101,15 @@ class BacnetClient:
             subscription.stop_subscription()
 
     def get_subscription(self, subscription_id: SubscriptionID) -> ObjectSubscription:
-
         return self.subscriptions[subscription_id]
 
     async def disconnect(self):
+        """
+        If a bacnet client was made on the initialisation of this class
+        (bacnet_client = None on object creation)
+        You should run this method when you are done with the python object
+        The python object will essentially be useless after this
+        """
 
         if self.borrowed_deivce:
             return
