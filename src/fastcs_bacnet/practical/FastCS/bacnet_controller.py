8c2fcd0c6f196f3ffaecc81738984ad99dc1c9b5
Oli Jones <oliver.jones@diamond.ac.uk>
2026-03-27 14:41:56 +0000

    docstrings and comments added, variable renamed

--- src/fastcs_bacnet/practical/FastCS/bacnet_controller.py
+++ src/fastcs_bacnet/practical/FastCS/bacnet_controller.py
@@ -6,17 +6,29 @@ from fastcs_bacnet.practical.FastCS.bacnet_subcontroller import BacnetSubControl
 
 
 class BacnetController(Controller):
+    """
+    A FastCS controller for bacnet subscriptions
+    """
+
     def __init__(self, bacnet_client: BacnetClient):
-        location_subscription_pairs = BacnetController._sort_subscriptions(
+        """
+        Creates a FastCS controller (and subcontrollers) from a BacnetClient object
+        bacnet_client: NOT a BAC0.lite object but a BacnetClient object
+            Will create subcontrollers for each device the BacnetClient is subscribed to
+            (these will be BacnetSubcontrollers)
+            Subcontroller naming convention: subcontroller_[index]
+            Where [index] increments from 0 for each device in no particular order
+        """
+        location_subscription_list_pairs = BacnetController._sort_subscriptions(
             bacnet_client.get_subscription_ids()
         )
 
         subcontroller_index = 0
-        for location in location_subscription_pairs.keys():
+        for location in location_subscription_list_pairs.keys():
             # change to a named tuple?
             device_ip_address = location[0]
             device_bacnet_port = location[1]
-            device_subscription_ids = location_subscription_pairs[location]
+            device_subscription_ids = location_subscription_list_pairs[location]
 
             device_controller = BacnetSubController(
                 bacnet_client,
@@ -35,14 +47,27 @@ class BacnetController(Controller):
     def _sort_subscriptions(
         subscription_ids: list[SubscriptionID],
     ) -> dict[tuple[str, int], list[SubscriptionID]]:
+        """
+        Sorts a list of subscription ids into lists of common locations (ip-port pairs)
+        subscription_ids: A list of SubscriptionID objects to sort
+            Each list of common locations is put into a dictionary where
+            the key is the location
+        """
 
-        location_subscription_pairs: dict[tuple[str, int], list[SubscriptionID]] = {}
+        location_subscription_list_pairs: dict[
+            tuple[str, int], list[SubscriptionID]
+        ] = {}
 
         for subscription_id in subscription_ids:
             location = (subscription_id.address, subscription_id.port)
-            if location in location_subscription_pairs:
-                location_subscription_pairs[location].append(subscription_id)
-            else:
-                location_subscription_pairs[location] = [subscription_id]
 
-        return location_subscription_pairs
+            # If the location is already in the dictionary
+            # add the subscription id to the list
+            if location in location_subscription_list_pairs:
+                location_subscription_list_pairs[location].append(subscription_id)
+            # If the location is not in the dictionary
+            # create a new list for the location, it contains the subscription id
+            else:
+                location_subscription_list_pairs[location] = [subscription_id]
+
+        return location_subscription_list_pairs
