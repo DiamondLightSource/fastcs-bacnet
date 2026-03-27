d790ed04f75cf9e5bc5417b19c9c014a84ade78b
Oli Jones <oliver.jones@diamond.ac.uk>
2026-03-27 13:44:01 +0000

    Bacnet subcontroller class written

--- src/fastcs_bacnet/practical/FastCS/bacnet_subcontroller.py
+++ src/fastcs_bacnet/practical/FastCS/bacnet_subcontroller.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 
 from fastcs.attributes import AttributeIO, AttributeIORef, AttrR
 from fastcs.controllers import Controller
+from fastcs.datatypes import Float
 from fastcs.util import ONCE
 
 from fastcs_bacnet.practical.BAC0.bacnet_client import BacnetClient
@@ -40,5 +41,34 @@ class AnalogOutputAttributeIO(AttributeIO[float, AnalogOutputAttributeIORef]):
 
 
 class BacnetSubController(Controller):
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        bacnet_client: BacnetClient,
+        ip_address: str,
+        port: int,
+        subscription_ids: list[SubscriptionID],
+    ):
+        super().__init__(ios=[AnalogOutputAttributeIO(bacnet_client)])
+
+        for subscription_id in subscription_ids:
+            # TODO: Throw an error here instead
+            if subscription_id.address != ip_address:
+                print(f"""
+                    Subcontroller address does not match subscription address
+                    Subcontroller address: {ip_address}
+                    Subscription address: {subscription_id.address}
+                """)
+            if subscription_id.port != port:
+                print(f"""
+                    Subcontroller port does not match subscription port
+                    Subcontroller port: {port}
+                    Subscription port: {subscription_id.port}
+                """)
+
+            self.add_attribute(
+                f"{subscription_id.object_type}_{subscription_id.object_id}",
+                AttrR(
+                    Float(),
+                    io_ref=AnalogOutputAttributeIORef(subscription_id=subscription_id),
+                ),
+            )
