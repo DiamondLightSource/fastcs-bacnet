d11850d32ee8e4fb719cf9d3e7a7f3d9510e8d52
Oli Jones <oliver.jones@diamond.ac.uk>
2026-03-30 14:36:42 +0000

    response timer class started

---
+++ src/fastcs_bacnet/diagnostics/BAC0/response_timer.py
@@ -0,0 +1,14 @@
+from fastcs_bacnet.dummy.BAC0.analog_output_object import AnalogOutputObject
+from fastcs_bacnet.practical.BAC0.object_subscription import ObjectSubscription
+
+
+class ResponseTimer:
+    def __init__(self):
+        pass
+
+    def add_subscription_pair(
+        self,
+        analog_output_object: AnalogOutputObject,
+        object_subscription: ObjectSubscription,
+    ):
+        pass
