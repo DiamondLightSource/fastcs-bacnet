dfed16e38601246cdfc31fcf0034f36fba66c3e4
Oli Jones <oliver.jones@diamond.ac.uk>
2026-03-30 10:18:49 +0000

    CA_polling example written

---
+++ src/fastcs_bacnet/examples/FastCS/CA_polling.py
@@ -0,0 +1,31 @@
+import asyncio
+
+from fastcs.launch import FastCS
+from fastcs.transports.epics import EpicsIOCOptions
+from fastcs.transports.epics.ca import EpicsCATransport
+
+from fastcs_bacnet.dummy.FastCS.polling_device_controller import PollingDeviceController
+from fastcs_bacnet.dummy.generic.device import Device as GenericDevice
+
+
+async def async_function():
+    dummy_device = GenericDevice(
+        "dummy",
+        number_of_constant_fields=10,
+        number_of_oscillating_fields=20,
+        number_of_random_fields=5,
+        number_of_read_write_fields=5,
+    )
+
+    epics_ca = EpicsCATransport(epicsca=EpicsIOCOptions(pv_prefix="DEMO"))
+
+    controller = PollingDeviceController(dummy_device)
+    fastcs = FastCS(controller, [epics_ca])
+
+    asyncio.create_task(fastcs.serve())
+
+    while True:
+        await asyncio.sleep(10)
+
+
+asyncio.run(async_function())
