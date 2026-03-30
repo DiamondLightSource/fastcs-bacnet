a301ea467be5118d8c10765ac74d0c45cf0df5a0
Oli Jones <oliver.jones@diamond.ac.uk>
2026-03-30 15:16:07 +0000

    privated fields

--- src/fastcs_bacnet/diagnostics/BAC0/subscription_pair.py
+++ src/fastcs_bacnet/diagnostics/BAC0/subscription_pair.py
@@ -8,22 +8,22 @@ from fastcs_bacnet.practical.BAC0.object_subscription import ObjectSubscription
 
 class SubscriptionPair:
     # subscription messages that have been sent but not recieved yet
-    sent_buffer: list[tuple[float, datetime]] = []
+    _sent_buffer: list[tuple[float, datetime]] = []
 
     # time between sending and recieving updates
     # size limited, only the most recent sent updates are kept
-    recent_receival_times: list[timedelta | None] = []
+    _recent_receival_times: list[timedelta | None] = []
 
     # max size of recent_recieval_times
-    recent_times_buffer_length: int
+    _recent_times_buffer_length: int
 
-    total_missed_updates: int = 0
-    total_updates_recieved: int = 0
-    total_update_wait_time: timedelta
+    _total_missed_updates: int = 0
+    _total_updates_recieved: int = 0
+    _total_update_wait_time: timedelta
 
     # when we cant match a recieved value to a sent value
     # shouldnt ever happed but we should definitely know if it does
-    total_mystery_updates: int = 0
+    _total_mystery_updates: int = 0
 
     def __init__(
         self,
@@ -31,8 +31,8 @@ class SubscriptionPair:
         object_subscription: ObjectSubscription,
         recent_times_buffer_length: int = 20,
     ):
-        self.recent_times_buffer_length = recent_times_buffer_length
-        self.total_update_wait_time = timedelta(0)
+        self._recent_times_buffer_length = recent_times_buffer_length
+        self._total_update_wait_time = timedelta(0)
 
         # maybe a bit cheeky to acess the analog_ouput_object underlying
         # device variable directly. Should be with a getter??
@@ -40,7 +40,7 @@ class SubscriptionPair:
         object_subscription.set_diagnostic_callback(self._on_recieve)
 
     def _on_send(self, new_value: float):
-        self.sent_buffer.append((new_value, datetime.now()))
+        self._sent_buffer.append((new_value, datetime.now()))
 
     def _on_recieve(self, property_identifier: str, property_value: float):
         if property_identifier is not PropertyIdentifier.presentValue:
@@ -50,39 +50,39 @@ class SubscriptionPair:
 
         sent_index = self._match(property_value)
         if sent_index is None:
-            self.total_mystery_updates += 1
+            self._total_mystery_updates += 1
             return
 
-        sent_time = self.sent_buffer[sent_index][1]
+        sent_time = self._sent_buffer[sent_index][1]
 
         # every sent update before the current one must have been missed
         # remove the sent buffer before the one we recieved
         # add
         for _ in range(sent_index):
-            self.sent_buffer.pop(0)
-            self.add_recieval_time(None)
+            self._sent_buffer.pop(0)
+            self._add_recieval_time(None)
 
-        self.sent_buffer.pop(0)
-        self.add_recieval_time(recieved_time - sent_time)
+        self._sent_buffer.pop(0)
+        self._add_recieval_time(recieved_time - sent_time)
 
     def _match(self, recieved_value: float) -> int | None:
 
-        for i in range(len(self.sent_buffer)):
-            sent_value_i = self.sent_buffer[i][0]
+        for i in range(len(self._sent_buffer)):
+            sent_value_i = self._sent_buffer[i][0]
             if sent_value_i == recieved_value:
                 return i
 
         return None
 
-    def add_recieval_time(self, receival_time: timedelta | None):
+    def _add_recieval_time(self, receival_time: timedelta | None):
 
         if receival_time is None:
-            self.total_missed_updates += 1
+            self._total_missed_updates += 1
         else:
-            self.total_updates_recieved += 1
-            self.total_update_wait_time += receival_time
+            self._total_updates_recieved += 1
+            self._total_update_wait_time += receival_time
 
-        self.recent_receival_times.append(receival_time)
+        self._recent_receival_times.append(receival_time)
 
-        while len(self.recent_receival_times) > self.recent_times_buffer_length:
-            self.recent_receival_times.pop(0)
+        while len(self._recent_receival_times) > self._recent_times_buffer_length:
+            self._recent_receival_times.pop(0)
