c9429e52b0029c79c4520fe03d626803932c947c
Oli Jones <oliver.jones@diamond.ac.uk>
2026-03-31 08:12:22 +0000

    added methods to get index and remove by index

--- src/fastcs_bacnet/diagnostics/BAC0/response_timer.py
+++ src/fastcs_bacnet/diagnostics/BAC0/response_timer.py
@@ -4,6 +4,9 @@ from fastcs_bacnet.practical.BAC0.object_subscription import ObjectSubscription
 
 
 class ResponseTimer:
+    # May want to change to a dictionary??
+    # Most of the operations invlolve iterating through it
+    # But getting a specific value using keys is also used
     subscription_pairs: list[SubscriptionPair] = []
 
     def __init__(self, recent_times_buffer_length: int = 20):
@@ -21,3 +24,29 @@ class ResponseTimer:
                 recent_times_buffer_length=self._recent_times_buffer_length,
             )
         )
+
+    def remove_subscription_pair(self, index: int):
+        subscription_pair = self.subscription_pairs.pop(index)
+        subscription_pair.stop_recording()
+
+    def index_from_device_object(
+        self, analog_output_object: AnalogOutputObject
+    ) -> int | None:
+        for index in range(len(self.subscription_pairs)):
+            index_analog_output_object = self.subscription_pairs[
+                index
+            ].get_analog_output_object()
+            if analog_output_object == index_analog_output_object:
+                return index
+        return None
+
+    def index_from_object_subscription(
+        self, object_subscription: ObjectSubscription
+    ) -> int | None:
+        for index in range(len(self.subscription_pairs)):
+            index_object_subscription = self.subscription_pairs[
+                index
+            ].get_object_subscription()
+            if object_subscription == index_object_subscription:
+                return index
+        return None
