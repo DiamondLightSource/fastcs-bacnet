564dac4e4565e339aa4232655d9ca79ef1aa68ea
Oli Jones <oliver.jones@diamond.ac.uk>
2026-03-31 08:00:54 +0000

    added method to add a pair

--- src/fastcs_bacnet/diagnostics/BAC0/response_timer.py
+++ src/fastcs_bacnet/diagnostics/BAC0/response_timer.py
@@ -1,14 +1,23 @@
+from fastcs_bacnet.diagnostics.BAC0.subscription_pair import SubscriptionPair
 from fastcs_bacnet.dummy.BAC0.analog_output_object import AnalogOutputObject
 from fastcs_bacnet.practical.BAC0.object_subscription import ObjectSubscription
 
 
 class ResponseTimer:
-    def __init__(self):
-        pass
+    subscription_pairs: list[SubscriptionPair] = []
+
+    def __init__(self, recent_times_buffer_length: int = 20):
+        self._recent_times_buffer_length = recent_times_buffer_length
 
     def add_subscription_pair(
         self,
         analog_output_object: AnalogOutputObject,
         object_subscription: ObjectSubscription,
     ):
-        pass
+        self.subscription_pairs.append(
+            SubscriptionPair(
+                analog_output_object,
+                object_subscription,
+                recent_times_buffer_length=self._recent_times_buffer_length,
+            )
+        )
