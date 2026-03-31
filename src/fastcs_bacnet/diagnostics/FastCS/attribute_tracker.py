0225fac5f3e81d63badf8fb1d460c5b33d2492d2
Oli Jones <oliver.jones@diamond.ac.uk>
2026-03-31 12:51:10 +0000

    attribute_tracker class started

---
+++ src/fastcs_bacnet/diagnostics/FastCS/attribute_tracker.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timedelta
+
+from fastcs_bacnet.diagnostics.FastCS.tracked_read_attribute import TrackedAttrR
+
+
+class AttributeTracker:
+    recent_times: list[timedelta]
+    first_update: datetime | None
+    last_update: datetime | None
+
+    def __init__(self, tracked_attribute: TrackedAttrR, history_size: int = 20):
+
+        self.tracked_attribute = tracked_attribute
+        self.history_size = history_size
+
+        self.tracked_attribute.set_diagnostic_callback(self.update_callback)
+
+    def update_callback(self, start_time: datetime, end_time: datetime):
+        pass
