9c2a9acdac3eb543f71fa33917da53cd99492012
Oli Jones <oliver.jones@diamond.ac.uk>
2026-03-31 09:28:55 +0000

    start method written

--- src/fastcs_bacnet/diagnostics/FastCS/queue_tracker.py
+++ src/fastcs_bacnet/diagnostics/FastCS/queue_tracker.py
@@ -1,3 +1,4 @@
+import asyncio
 from datetime import datetime
 
 
@@ -9,18 +10,25 @@ class QueueTracker:
 
     recent_queue_history: list[tuple[int, datetime]]
     history_size: int
-    poll_rate: float
+    poll_period: float
     total_lost_requests: int = 0
 
     def __init__(
         self, fastcs_coroutine=None, poll_rate: float = 1.0, history_size: int = 20
     ):
         self.fastcs_coroutine = fastcs_coroutine
-        self.poll_rate = poll_rate
+        self.poll_period = poll_rate
         self.history_size = history_size
 
-    def start(self):
-        pass
+    async def start(self):
+
+        if self.fastcs_coroutine is not None:
+            self.fastcs_start_time = datetime.now()
+            asyncio.create_task(self.fastcs_coroutine())
+
+        while True:
+            self.poll()
+            await asyncio.sleep(self.poll_period)
 
     def poll(self):
         pass
