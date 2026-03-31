c4b3bff5267c4f4fec10b440e6801173fa53b7a9
Oli Jones <oliver.jones@diamond.ac.uk>
2026-03-31 09:41:44 +0000

    poll and time set methods implemented

--- src/fastcs_bacnet/diagnostics/FastCS/queue_tracker.py
+++ src/fastcs_bacnet/diagnostics/FastCS/queue_tracker.py
@@ -1,6 +1,8 @@
 import asyncio
 from datetime import datetime
 
+from fastcs_bacnet.diagnostics.FastCS.queue_probe import QueueStatsProbe
+
 
 class QueueTracker:
     fastcs_start_time: datetime | None = None
@@ -19,6 +21,7 @@ class QueueTracker:
         self.fastcs_coroutine = fastcs_coroutine
         self.poll_period = poll_rate
         self.history_size = history_size
+        self.probe = QueueStatsProbe()
 
     async def start(self):
 
@@ -31,7 +34,31 @@ class QueueTracker:
             await asyncio.sleep(self.poll_period)
 
     def poll(self):
-        pass
+        queue_stats_data = self.probe.get_stats()
+        stats_time = datetime.now()
 
-    def set_fastcs_start_time(self, fast_cs_start_time: datetime):
-        pass
+        self.recent_queue_history.append((queue_stats_data.queue_length[0], stats_time))
+
+        while len(self.recent_queue_history) > self.history_size:
+            self.recent_queue_history.pop(0)
+
+        if self.first_queue_time is None and queue_stats_data.queue_length[0] > 0:
+            self.first_queue_time = stats_time
+
+        if queue_stats_data.queue_overflow[0] != self.total_lost_requests:
+            self.last_overflow_time = stats_time
+            # not sure if the queue stats data gives total overflows
+            # or current overflows
+            self.total_lost_requests = queue_stats_data.queue_overflow[0]
+
+            if self.first_overflow_time is None:
+                self.first_overflow_time = stats_time
+
+    def set_fastcs_start_time(self, fastcs_start_time: datetime):
+        if self.fastcs_coroutine is not None:
+            print("start time will be set when start method is run")
+            return
+        if self.fastcs_start_time is not None:
+            print("start time already set")
+            return
+        self.fastcs_start_time = fastcs_start_time
