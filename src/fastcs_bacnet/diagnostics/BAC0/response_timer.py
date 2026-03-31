2609d7cd952c59ed8c359947d73a0cd718012ca6
Oli Jones <oliver.jones@diamond.ac.uk>
2026-03-31 08:19:50 +0000

    docstrings added

--- src/fastcs_bacnet/diagnostics/BAC0/response_timer.py
+++ src/fastcs_bacnet/diagnostics/BAC0/response_timer.py
@@ -4,6 +4,10 @@ from fastcs_bacnet.practical.BAC0.object_subscription import ObjectSubscription
 
 
 class ResponseTimer:
+    """
+    Class for managing a list of SubscriptionPair s
+    """
+
     # May want to change to a dictionary??
     # Most of the operations invlolve iterating through it
     # But getting a specific value using keys is also used
@@ -17,6 +21,9 @@ class ResponseTimer:
         analog_output_object: AnalogOutputObject,
         object_subscription: ObjectSubscription,
     ):
+        """
+        Creates a subscription pair from the input objects and adds it to the list
+        """
         self.subscription_pairs.append(
             SubscriptionPair(
                 analog_output_object,
@@ -26,12 +33,20 @@ class ResponseTimer:
         )
 
     def remove_subscription_pair(self, index: int):
+        """
+        Removes a subscription pair from the list at a specific index
+        Stops the subscription
+        Find index of a pair using index_from methods
+        """
         subscription_pair = self.subscription_pairs.pop(index)
         subscription_pair.stop_recording()
 
     def index_from_device_object(
         self, analog_output_object: AnalogOutputObject
     ) -> int | None:
+        """
+        Gets index of a subscription pair from its analog_output_object
+        """
         for index in range(len(self.subscription_pairs)):
             index_analog_output_object = self.subscription_pairs[
                 index
@@ -43,6 +58,9 @@ class ResponseTimer:
     def index_from_object_subscription(
         self, object_subscription: ObjectSubscription
     ) -> int | None:
+        """
+        Gets index of a subscription pair from its object_subscription
+        """
         for index in range(len(self.subscription_pairs)):
             index_object_subscription = self.subscription_pairs[
                 index
