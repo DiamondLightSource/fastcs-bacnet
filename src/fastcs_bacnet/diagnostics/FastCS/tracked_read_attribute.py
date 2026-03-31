aba7ba9a928f6deb787ea3ec076a58d3875083f2
Oli Jones <oliver.jones@diamond.ac.uk>
2026-03-31 12:19:32 +0000

    docstrings added to tracked_read_attribute

--- src/fastcs_bacnet/diagnostics/FastCS/tracked_read_attribute.py
+++ src/fastcs_bacnet/diagnostics/FastCS/tracked_read_attribute.py
@@ -13,19 +13,32 @@ from fastcs.datatypes import DType_T
 # In that case we couldnt add our own callback
 # as were measuring the time it takes to call the callbacks
 class TrackedAttrR(AttrR[DType_T, AttributeIORefT]):
-    diagnostic_callback: Callable[[dt, dt], None] | None
+    """
+    Extension of the fastcs AttrR class
+    Overrides the update method to record start and finish times
+    Calls a callback with the times after
+    """
+
+    _diagnostic_callback: Callable[[dt, dt], None] | None
 
     async def update(self, value: Any) -> None:
+        """
+        Overridden update method
+        Records start time
+        Waits for the original update method to call
+        Records end time
+        Then calls callback with the times as arguments
+        """
         update_start_time = dt.now()
 
         await super().update(value)
 
         update_end_time = dt.now()
 
-        if self.diagnostic_callback is not None:
-            self.diagnostic_callback(update_start_time, update_end_time)
+        if self._diagnostic_callback is not None:
+            self._diagnostic_callback(update_start_time, update_end_time)
 
     def set_diagnostic_callback(
         self, diagnostic_callback: Callable[[dt, dt], None] | None
     ):
-        self.diagnostic_callback = diagnostic_callback
+        self._diagnostic_callback = diagnostic_callback
