3089399c1253650d1443ac30aaed4efc80a35f1a
Oli Jones <oliver.jones@diamond.ac.uk>
2026-04-02 09:51:33 +0000

    update loops dont start automatically

--- src/fastcs_bacnet/dummy/generic/device_variables/puppet_variable/puppet_controller.py
+++ src/fastcs_bacnet/dummy/generic/device_variables/puppet_variable/puppet_controller.py
@@ -18,7 +18,8 @@ class PuppetController:
         self, initial_variables: list[PuppetVariable], update_loops: int = 1, **kwargs
     ):
         """
-        Adds all initial PuppetVariables and starts the update loops
+        Adds all initial PuppetVariables
+        Does NOT start the update loop
         initial_variables: Variables to be controller by this controller
             PuppetVariables should only be controlled by one controller
         update_loops: The number of RandomVariables to start
@@ -26,21 +27,23 @@ class PuppetController:
         """
 
         self.variables = []
+        self.update_loops_started = False
 
         for puppet_variable in initial_variables:
             self.add_puppet_variable(puppet_variable)
 
-        self._start_update_loops(update_loops, **kwargs)
-
     def add_puppet_variable(self, puppet_variable: PuppetVariable):
         self.variables.append(puppet_variable)
 
-    def _start_update_loops(self, update_loops: int, **kwargs):
+    def start_update_loops(self, update_loops: int, **kwargs):
         """
         Creates update_loops s RandomVariables
         Sets the callback to _update_random_variable
         keyword arguments are sent straight to the RandomVariable constructor
         """
+        if self.update_loops_started:
+            return
+        self.update_loops_started = True
 
         for i in range(update_loops):
             RandomVariable(
