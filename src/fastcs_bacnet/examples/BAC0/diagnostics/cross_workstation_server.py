f19a89527c78ed7d58fbe1ec8478ad719a413526
Oli Jones <oliver.jones@diamond.ac.uk>
2026-04-14 08:26:19 +0000

    Change server objects to random

--- src/fastcs_bacnet/examples/BAC0/diagnostics/cross_workstation_server.py
+++ src/fastcs_bacnet/examples/BAC0/diagnostics/cross_workstation_server.py
@@ -3,12 +3,14 @@ import asyncio
 from fastcs_bacnet.dummy.BAC0.device import Device
 
 DUMMY_DEVICE_PORT = 47808
-DUMMY_DEVICE_ID = 125
+DUMMY_DEVICE_ID = 998
 
 
 async def async_function():
 
-    dummy_device_0 = Device(None, 47808, 998, number_of_constant_fields=10)
+    dummy_device_0 = Device(
+        None, DUMMY_DEVICE_PORT, DUMMY_DEVICE_ID, number_of_random_fields=10
+    )
 
     await asyncio.Event().wait()
 
