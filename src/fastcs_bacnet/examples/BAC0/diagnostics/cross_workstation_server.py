d833d8f8d31375efe764f3236c3779bbe94dea79
Oli Jones <oliver.jones@diamond.ac.uk>
2026-04-15 08:38:49 +0000

    Make multiple devices in cross_workstation_server

--- src/fastcs_bacnet/examples/BAC0/diagnostics/cross_workstation_server.py
+++ src/fastcs_bacnet/examples/BAC0/diagnostics/cross_workstation_server.py
@@ -2,19 +2,25 @@ import asyncio
 
 from fastcs_bacnet.dummy.BAC0.device import Device
 
-DUMMY_DEVICE_PORT = 47808
-DUMMY_DEVICE_ID = 998
+DUMMY_DEVICE_PORTS = []
+DUMMY_DEVICE_IDS = []
+NUMBER_OF_FIELDS = 0
 
 
 async def async_function():
+    dummy_devices = []
 
-    dummy_device_0 = Device(
-        None, DUMMY_DEVICE_PORT, DUMMY_DEVICE_ID, number_of_random_fields=10
-    )
+    for i in range(len(DUMMY_DEVICE_PORTS)):
+        port = DUMMY_DEVICE_PORTS[i]
+        id = DUMMY_DEVICE_IDS[i]
+
+        dummy_device = Device(None, port, id, number_of_random_fields=NUMBER_OF_FIELDS)
+        dummy_devices.append(dummy_device)
 
     await asyncio.Event().wait()
 
-    await dummy_device_0.disconnect()
+    for dummy_device in dummy_devices:
+        await dummy_device.disconnect()
 
 
 asyncio.run(async_function())
