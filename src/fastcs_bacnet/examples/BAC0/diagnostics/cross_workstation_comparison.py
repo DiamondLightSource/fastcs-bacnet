bdc3d8efe130db02cec8c2706e53f2fa34056652
Oli Jones <oliver.jones@diamond.ac.uk>
2026-04-15 09:16:01 +0000

    Process client txt file

--- src/fastcs_bacnet/examples/BAC0/diagnostics/cross_workstation_comparison.py
+++ src/fastcs_bacnet/examples/BAC0/diagnostics/cross_workstation_comparison.py
@@ -10,18 +10,24 @@ def compare_files(bacnet_server_filepath: str, wireshark_filepath: str):
     # not sure what to do with dataframe yet
     print(wireshark_dataframe.info())
 
-    start_time = None  # noqa: F841
-    end_time = None  # noqa: F841
+    start_time = None
+    end_time = None
+
+    bacnet_server_frequencies: dict[tuple[str, int, int], int] = defaultdict(int)
 
     line = ""
     while "END" not in line:
         line = bacnet_server_file.readline()
         if "." in line:
-            # data line
-            pass
-        elif "," in line:
-            # metadata line
-            pass
+            socket_address = line.split(",")[0]
+            address = socket_address.split(":")[0]
+            port = int(socket_address.split(":")[1])
+            object_id = int(line.split(",")[1].split(".")[0])
 
-    bacnet_server_frequencies: dict[tuple[str, int, int], int] = defaultdict(int)  # noqa: F841
+            bacnet_server_frequencies[(address, port, object_id)] += 1
+
+        elif "," in line:
+            start_time = line.split(",")[0]  # noqa: F841
+            end_time = line.split(",")[1]  # noqa: F841
+
     wireshark_package_frequencies: dict[tuple[str, int, int], int] = {}  # noqa: F841
