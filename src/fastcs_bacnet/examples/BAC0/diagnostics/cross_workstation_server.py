8f418fc753a3a84720ab0bed6d848475ec4010cf
Oli Jones <oliver.jones@diamond.ac.uk>
2026-04-15 13:29:56 +0000

    Update server to serve 2000 updates per minute

--- src/fastcs_bacnet/examples/BAC0/diagnostics/cross_workstation_server.py
+++ src/fastcs_bacnet/examples/BAC0/diagnostics/cross_workstation_server.py
@@ -2,19 +2,28 @@ import asyncio
 
 from fastcs_bacnet.dummy.BAC0.device import Device
 
-DUMMY_DEVICE_PORTS = []
-DUMMY_DEVICE_IDS = []
-NUMBER_OF_FIELDS = 0
 
-
-async def async_function():
+async def run_devices(
+    dummy_device_ports: list[int],
+    dummy_device_ids: list[int],
+    number_of_fields: int,
+    min_change_time: float = 1.0,
+    max_change_time=5.0,
+):
     dummy_devices = []
 
-    for i in range(len(DUMMY_DEVICE_PORTS)):
-        port = DUMMY_DEVICE_PORTS[i]
-        id = DUMMY_DEVICE_IDS[i]
+    for i in range(len(dummy_device_ports)):
+        port = dummy_device_ports[i]
+        id = dummy_device_ids[i]
 
-        dummy_device = Device(None, port, id, number_of_random_fields=NUMBER_OF_FIELDS)
+        dummy_device = Device(
+            None,
+            port,
+            id,
+            number_of_random_fields=number_of_fields,
+            min_change_time=min_change_time,
+            max_change_time=max_change_time,
+        )
         dummy_devices.append(dummy_device)
 
     await asyncio.Event().wait()
@@ -23,4 +32,17 @@ async def async_function():
         await dummy_device.disconnect()
 
 
-asyncio.run(async_function())
+# constants chosen to simulate roughly 2000 updates per minute
+DUMMY_DEVICE_PORTS = list(range(47808, 47820))
+DUMMY_DEVICE_IDS = list(range(123, 135))
+NUMBER_OF_FIELDS = 30
+
+asyncio.run(
+    run_devices(
+        DUMMY_DEVICE_PORTS,
+        DUMMY_DEVICE_IDS,
+        NUMBER_OF_FIELDS,
+        min_change_time=2.0,
+        max_change_time=10.0,
+    )
+)
