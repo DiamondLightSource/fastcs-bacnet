a9c345a3f8839a864ed43b48fd6b34f0ac3fc173
Oli Jones <oliver.jones@diamond.ac.uk>
2026-04-17 13:50:30 +0000

    Implement callback stack in device_variable

--- src/fastcs_bacnet/dummy/generic/device_variables/device_variable.py
+++ src/fastcs_bacnet/dummy/generic/device_variables/device_variable.py
@@ -1,6 +1,8 @@
 from abc import ABC
 from collections.abc import Callable
 
+from fastcs_bacnet.practical.generic.callback_stack import CallbackStack
+
 
 class DeviceVariable(ABC):
     """
@@ -12,10 +14,12 @@ class DeviceVariable(ABC):
     """
 
     _value: float | None
-    diagnostic_callback: Callable[[float | None, float], None] | None = None
+    callback_stack: CallbackStack[float | None, float]
 
     def __init__(
-        self, name: str, update_callback: Callable[[float], None] | None = None
+        self,
+        name: str,
+        update_callback: Callable[[float | None, float], None] | None = None,
     ):
         """
         name: name of variable, should be unique per device
@@ -23,30 +27,13 @@ class DeviceVariable(ABC):
         """
         self._value = None
         self.name = name
-        self.update_callback = update_callback
+
+        self.callback_stack = CallbackStack[float | None, float]()
+        if update_callback is not None:
+            self.callback_stack.add_to_stack(update_callback)
 
     def get_value(self) -> float | None:
         """
         Returns value of variable
         """
         return self._value
-
-    def set_update_callback(self, update_callback: Callable[[float], None] | None):
-        """
-        Changes the callback procedure of this variable
-        Use argument None to remove the current callback
-        This will affect updates immediately after the method is called
-        """
-        self.update_callback = update_callback
-
-    def set_diagnostic_callback(
-        self, diagnostic_callback: Callable[[float | None, float], None] | None
-    ):
-        """
-        Changes the diagnostic callback procedure of this variable
-        Use argument None to remove the current callback
-        There is no difference between the calling of the update callback
-        and diagnostic callback. There are just 2 slots to make setting and removing
-        the separate callbacks easier
-        """
-        self.diagnostic_callback = diagnostic_callback
