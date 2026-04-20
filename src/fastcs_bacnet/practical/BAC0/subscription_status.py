2653d4ea5567074c73a49e105366d0c474057212
Oli Jones <oliver.jones@diamond.ac.uk>
2026-04-20 08:25:26 +0000

    Make subscription status class

---
+++ src/fastcs_bacnet/practical/BAC0/subscription_status.py
@@ -0,0 +1,39 @@
+from enum import Enum
+
+from fastcs_bacnet.practical.BAC0.subscription_id import SubscriptionID
+from fastcs_bacnet.practical.generic.callback_stack import CallbackStack
+
+
+class Status(Enum):
+    NEITHER = ""
+    RED_UP = "R"
+    BLUE_UP = "B"
+    BOTH = "RB"
+
+
+class Team(Enum):
+    RED = "R"
+    BLUE = "B"
+
+
+class SubscriptionStatus:
+    subscription_id: SubscriptionID
+    callback: CallbackStack
+    status: Status = Status.NEITHER
+    callback_called: Status = Status.NEITHER
+
+    def __init__(self, callback: CallbackStack, subscription_id: SubscriptionID):
+        self.callback = callback
+        self.subscription_id = subscription_id
+
+    def is_red_up(self) -> bool:
+        return "R" in self.status.value
+
+    def is_blue_up(self) -> bool:
+        return "B" in self.status.value
+
+    def get_status(self) -> Status:
+        return self.status
+
+    def get_callback_called(self) -> Status:
+        return self.callback_called
