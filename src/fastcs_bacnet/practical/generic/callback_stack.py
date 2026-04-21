136319bc27569ba22c9d0b3b4ce971d8894909fb
Oli Jones <oliver.jones@diamond.ac.uk>
2026-04-21 08:31:34 +0000

    Private fields in callback_stack

--- src/fastcs_bacnet/practical/generic/callback_stack.py
+++ src/fastcs_bacnet/practical/generic/callback_stack.py
@@ -16,18 +16,18 @@ class CallbackStack(Generic[P]):
     Adding callbacks that manipulate common state is undefined behaviour
     """
 
-    sync_callbacks: list[Callable[P, None]]
-    async_callbacks: list[Callable[P, Coroutine[None, None, None]]]
-    in_progress_callbacks: set[asyncio.Task]
+    _sync_callbacks: list[Callable[P, None]]
+    _async_callbacks: list[Callable[P, Coroutine[None, None, None]]]
+    _in_progress_callbacks: set[asyncio.Task]
 
-    next_callback_index: int = 0
-    callback_dict: dict[int, Callback]
+    _next_callback_index: int = 0
+    _callback_dict: dict[int, Callback]
 
     def __init__(self):
-        self.sync_callbacks = []
-        self.async_callbacks = []
-        self.in_progress_callbacks = set()
-        self.callback_dict = {}
+        self._sync_callbacks = []
+        self._async_callbacks = []
+        self._in_progress_callbacks = set()
+        self._callback_dict = {}
 
     def add_to_stack(self, f: Callback) -> int:
         """
@@ -37,18 +37,18 @@ class CallbackStack(Generic[P]):
         """
 
         if iscoroutinefunction(f):
-            self.async_callbacks.append(f)
+            self._async_callbacks.append(f)
         elif callable(f):
             # We know f must be a normal callable function here (not a coroutine)
             # as the previous if establishes it is not a coroutine
             # Pylance does not pick this up though
-            self.sync_callbacks.append(f)  # type: ignore
+            self._sync_callbacks.append(f)  # type: ignore
 
-        callback_key = self.next_callback_index
+        callback_key = self._next_callback_index
         # TODO: make a better key system that wont eventually overflow
-        self.next_callback_index += 1
+        self._next_callback_index += 1
 
-        self.callback_dict[callback_key] = f
+        self._callback_dict[callback_key] = f
 
         return callback_key
 
@@ -56,15 +56,15 @@ class CallbackStack(Generic[P]):
         """
         Returns callback when given the key
         """
-        if key not in self.callback_dict:
+        if key not in self._callback_dict:
             return None
-        return self.callback_dict[key]
+        return self._callback_dict[key]
 
     def remove_callback(self, key: int):
         """
         Removes a callback when given the key
         """
-        self.callback_dict.pop(key)
+        self._callback_dict.pop(key)
 
     def sum_callback(self, *args: P.args, **kwargs: P.kwargs):
         """
@@ -74,10 +74,10 @@ class CallbackStack(Generic[P]):
         one by one
         """
 
-        for async_callback in self.async_callbacks:
+        for async_callback in self._async_callbacks:
             task = asyncio.create_task(async_callback(*args, **kwargs))
-            self.in_progress_callbacks.add(task)
-            task.add_done_callback(self.in_progress_callbacks.discard)
+            self._in_progress_callbacks.add(task)
+            task.add_done_callback(self._in_progress_callbacks.discard)
 
-        for sync_callback in self.sync_callbacks:
+        for sync_callback in self._sync_callbacks:
             sync_callback(*args, **kwargs)
