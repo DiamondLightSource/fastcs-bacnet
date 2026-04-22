550dbe38916c3cb489475c416af3410f1ff66dd0
Oli Jones <oliver.jones@diamond.ac.uk>
2026-04-22 10:49:35 +0000

    Put repeated code into functions

--- tests/unit_tests/test_update_handler.py
+++ tests/unit_tests/test_update_handler.py
@@ -1,4 +1,5 @@
 import asyncio
+from collections.abc import Callable
 
 from fastcs_bacnet.practical.BAC0.subscriptions.subscription_id import (
     IPv4SocketAddress,
@@ -16,13 +17,14 @@ from fastcs_bacnet.practical.BAC0.subscriptions.update_handler import (
 from fastcs_bacnet.practical.generic.callback_stack import CallbackStack
 
 
-def test_single_update_callback():
-
+def blank_update_handler_with_callback_counter(
+    blank_update_callback: Callable[[], bool],
+):
     callback_stack = CallbackStack[str, float]()
-    callback_pointer = ["Not Called"]
+    callback_counter = [0]
 
     def callback(property_identifier: str, property_value: float):
-        callback_pointer[0] = "Called"
+        callback_counter[0] += 1
 
     callback_stack.add_to_stack(callback)
 
@@ -31,7 +33,35 @@ def test_single_update_callback():
     )
     status = SubscriptionStatus(callback_stack, subscription_id, 60)
 
-    update_handler = UpdateHandler(Team.BLUE, status, lambda: False)
+    update_handler = UpdateHandler(Team.BLUE, status, blank_update_callback)
+
+    return (update_handler, callback_counter)
+
+
+async def call_callback_and_check_result(
+    update_handler: UpdateHandler,
+    callback_counter: list[int],
+    expected_pointer_value: int,
+    expected_status: Status,
+):
+    update_handler.callback("present-value", 0)
+
+    # have to wait as callback creates a task
+    await asyncio.sleep(0.5)
+
+    # should not have changed as there was no race
+    assert update_handler.status.get_callback_called() == expected_status
+
+    # callback should have been called
+    assert callback_counter[0] == expected_pointer_value
+
+
+def test_single_update_callback():
+
+    update_handler, callback_counter = blank_update_handler_with_callback_counter(
+        lambda: False
+    )
+    status = update_handler.status
 
     # shoudnt matter but just making sure for future updates
     status.set_team_up(Team.BLUE)
@@ -39,166 +69,101 @@ def test_single_update_callback():
     # already tested but a necessary start for this test
     assert status.get_callback_called() == Status.NEITHER
 
-    async def run_callback_and_check_result():
-        update_handler.callback("present-value", 0)
-
-        # have to wait as callback creates a task
-        await asyncio.sleep(0.5)
-
-        # should not have changed as there was no race
-        assert status.get_callback_called() == Status.NEITHER
-
-        # callback should have been called
-        assert callback_pointer[0] == "Called"
-
-    asyncio.run(run_callback_and_check_result())
+    # when update_handler.callback is called, counter should be incremented by 1
+    # and Status should stay as NEITHER
+    asyncio.run(
+        call_callback_and_check_result(
+            update_handler, callback_counter, 1, Status.NEITHER
+        )
+    )
 
 
 def test_expect_blank_without_callback():
-    callback_stack = CallbackStack[str, float]()
-    callback_pointer = ["Not Called"]
-
-    def callback(property_identifier: str, property_value: float):
-        callback_pointer[0] = "Called"
-
-    callback_stack.add_to_stack(callback)
-
-    subscription_id = SubscriptionID(
-        IPv4SocketAddress("127.0.0.1", 47808), ObjectIdentifier("analog-output", 0)
+    update_handler, callback_counter = blank_update_handler_with_callback_counter(
+        lambda: False
     )
-    status = SubscriptionStatus(callback_stack, subscription_id, 60)
-
-    def blank_update_callback():
-        return False
-
-    update_handler = UpdateHandler(Team.BLUE, status, blank_update_callback)
-
-    update_handler.expect_blank_update()
+    status = update_handler.status
 
     # shoudnt matter but just making sure for future updates
     status.set_team_up(Team.BLUE)
 
-    async def run_callback_and_check_result():
-        update_handler.callback("present-value", 0)
+    update_handler.expect_blank_update()
 
-        # have to wait as callback creates a task
-        await asyncio.sleep(0.5)
-
-        # callback should NOT have been called as it was a blank update
-        # AND the blank callback returned False
-        assert callback_pointer[0] == "Not Called"
-
-    asyncio.run(run_callback_and_check_result())
+    # when update_handler.callback is called, counter should be NOT incremented by 1
+    # This is because a blank update was expected and the
+    # blank_update_callback returns False
+    # and Status should stay as NEITHER
+    asyncio.run(
+        call_callback_and_check_result(
+            update_handler, callback_counter, 0, Status.NEITHER
+        )
+    )
 
 
 def test_expect_blank_with_callback():
-    callback_stack = CallbackStack[str, float]()
-    callback_pointer = ["Not Called"]
-
-    def callback(property_identifier: str, property_value: float):
-        callback_pointer[0] = "Called"
-
-    callback_stack.add_to_stack(callback)
-
-    subscription_id = SubscriptionID(
-        IPv4SocketAddress("127.0.0.1", 47808), ObjectIdentifier("analog-output", 0)
+    update_handler, callback_counter = blank_update_handler_with_callback_counter(
+        lambda: True
     )
-    status = SubscriptionStatus(callback_stack, subscription_id, 60)
-
-    update_handler = UpdateHandler(Team.BLUE, status, lambda: True)
-
-    update_handler.expect_blank_update()
+    status = update_handler.status
 
     # shoudnt matter but just making sure for future updates
     status.set_team_up(Team.BLUE)
 
-    async def run_callback_and_check_result():
-        update_handler.callback("present-value", 0)
+    update_handler.expect_blank_update()
 
-        # have to wait as callback creates a task
-        await asyncio.sleep(0.5)
-
-        # callback should have been called as it was a blank update
-        # AND the blank callback returned True
-        assert callback_pointer[0] == "Called"
-
-    asyncio.run(run_callback_and_check_result())
+    # when update_handler.callback is called, counter should be incremented by 1
+    # This is because a blank update was expected and the
+    # blank_update_callback returns True
+    # and Status should stay as NEITHER
+    asyncio.run(
+        call_callback_and_check_result(
+            update_handler, callback_counter, 1, Status.NEITHER
+        )
+    )
 
 
 def test_one_at_a_time():
-    callback_stack = CallbackStack[str, float]()
-    callback_pointer = ["Not Called"]
-
-    def callback(property_identifier: str, property_value: float):
-        if callback_pointer[0] == "Not Called":
-            callback_pointer[0] = "Called"
-        elif callback_pointer[0] == "Called":
-            callback_pointer[0] = "Called Twice"
-
-    callback_stack.add_to_stack(callback)
-
-    subscription_id = SubscriptionID(
-        IPv4SocketAddress("127.0.0.1", 47808), ObjectIdentifier("analog-output", 0)
+    update_handler_blue, callback_counter = blank_update_handler_with_callback_counter(
+        lambda: False
     )
-    status = SubscriptionStatus(callback_stack, subscription_id, 60)
+    status = update_handler_blue.status
 
-    update_handler_blue = UpdateHandler(Team.BLUE, status, lambda: False)
     update_handler_red = UpdateHandler(Team.RED, status, lambda: False)
 
     # need to both be up so status is prepared for a race
     status.set_team_up(Team.BLUE)
     status.set_team_up(Team.RED)
 
-    async def call_callbacks_one_at_a_time():
-        update_handler_blue.callback("present-value", 0)
+    # callback should be called as blue "won" the race (counter === 1)
+    # callback status should also reflect blue won the race
+    asyncio.run(
+        call_callback_and_check_result(
+            update_handler_blue, callback_counter, 1, Status.BLUE_UP
+        )
+    )
 
-        # have to wait as callback creates a task
-        await asyncio.sleep(0.5)
-
-        assert callback_pointer[0] == "Called"
-
-        # status should reflect that blue got their first
-        assert status.get_callback_called() == Status.BLUE_UP
-
-        update_handler_red.callback("present-value", 0)
-
-        # have to wait as callback creates a task
-        await asyncio.sleep(0.5)
-
-        # callback should not have been called again
-        assert callback_pointer[0] == "Called"
-
-        # status should be reset for the next race
-        assert status.get_callback_called() == Status.BOTH
-
-    asyncio.run(call_callbacks_one_at_a_time())
+    # callback should not be called again as it was already called
+    # callback status should be reset for the next race
+    asyncio.run(
+        call_callback_and_check_result(
+            update_handler_red, callback_counter, 1, Status.BOTH
+        )
+    )
 
 
 def test_race():
-    callback_stack = CallbackStack[str, float]()
-    callback_pointer = ["Not Called"]
-
-    def callback(property_identifier: str, property_value: float):
-        if callback_pointer[0] == "Not Called":
-            callback_pointer[0] = "Called"
-        elif callback_pointer[0] == "Called":
-            callback_pointer[0] = "Called Twice"
-
-    callback_stack.add_to_stack(callback)
-
-    subscription_id = SubscriptionID(
-        IPv4SocketAddress("127.0.0.1", 47808), ObjectIdentifier("analog-output", 0)
+    update_handler_blue, callback_counter = blank_update_handler_with_callback_counter(
+        lambda: False
     )
-    status = SubscriptionStatus(callback_stack, subscription_id, 60)
+    status = update_handler_blue.status
 
-    update_handler_blue = UpdateHandler(Team.BLUE, status, lambda: False)
     update_handler_red = UpdateHandler(Team.RED, status, lambda: False)
 
     # need to both be up so status is prepared for a race
     status.set_team_up(Team.BLUE)
     status.set_team_up(Team.RED)
 
-    async def call_callbacks_one_at_a_time():
+    async def call_callbacks_simultaneously():
         update_handler_blue.callback("present-value", 0)
         update_handler_red.callback("present-value", 0)
 
@@ -206,29 +171,19 @@ def test_race():
         await asyncio.sleep(0.5)
 
         # callback should have been called once
-        assert callback_pointer[0] == "Called"
+        assert callback_counter[0] == 1
 
         # status should be reset for the next race
         assert status.get_callback_called() == Status.BOTH
 
-    asyncio.run(call_callbacks_one_at_a_time())
+    asyncio.run(call_callbacks_simultaneously())
 
 
 def test_stop_update_handler():
-    callback_stack = CallbackStack[str, float]()
-    callback_pointer = ["Not Called"]
-
-    def callback(property_identifier: str, property_value: float):
-        callback_pointer[0] = "Called"
-
-    callback_stack.add_to_stack(callback)
-
-    subscription_id = SubscriptionID(
-        IPv4SocketAddress("127.0.0.1", 47808), ObjectIdentifier("analog-output", 0)
+    update_handler, callback_counter = blank_update_handler_with_callback_counter(
+        lambda: False
     )
-    status = SubscriptionStatus(callback_stack, subscription_id, 60)
-
-    update_handler = UpdateHandler(Team.BLUE, status, lambda: False)
+    status = update_handler.status
 
     # shoudnt matter but just making sure for future updates
     status.set_team_up(Team.BLUE)
@@ -238,16 +193,9 @@ def test_stop_update_handler():
     # already tested but a necessary start for this test
     assert status.get_callback_called() == Status.NEITHER
 
-    async def run_callback_and_check_result():
-        update_handler.callback("present-value", 0)
-
-        # have to wait as callback creates a task
-        await asyncio.sleep(0.5)
-
-        # should not have changed as there was no race
-        assert status.get_callback_called() == Status.NEITHER
-
-        # callback should have been called
-        assert callback_pointer[0] == "Not Called"
-
-    asyncio.run(run_callback_and_check_result())
+    # callback should not be called as its been stooped
+    asyncio.run(
+        call_callback_and_check_result(
+            update_handler, callback_counter, 0, Status.NEITHER
+        )
+    )
