779532ba8feba9a48ccda845c84c9aed6b31f2c4
Oli Jones <oliver.jones@diamond.ac.uk>
2026-04-29 15:11:52 +0000

    Make a project script that connects to full program

--- src/fastcs_bacnet/__main__.py
+++ src/fastcs_bacnet/__main__.py
@@ -1,7 +1,11 @@
 """Interface for ``python -m fastcs_bacnet``."""
 
+import asyncio
 from argparse import ArgumentParser
 from collections.abc import Sequence
+
+from fastcs_bacnet.core.csv_parser import parse_csv
+from fastcs_bacnet.core.fastcs_bacnet import fastcs_bacnet
 
 from . import __version__
 
@@ -17,7 +21,23 @@ def main(args: Sequence[str] | None = None) -> None:
         action="version",
         version=__version__,
     )
-    parser.parse_args(args)
+    parser.add_argument(
+        "-f",
+        "--file_path",
+        type=str,
+        help="Filepath to the EDE file (or dummy equivalent)",
+    )
+    python_argument_object = parser.parse_args(args)
+
+    if python_argument_object.file_path is None:
+        print("file path cannot be None")
+        return
+
+    subscription_ids = parse_csv(python_argument_object.file_path)
+    if len(subscription_ids) == 0:
+        return
+
+    asyncio.run(fastcs_bacnet(subscription_ids))
 
 
 if __name__ == "__main__":
