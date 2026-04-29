714d3ac4114a3227ef151985cb0f5f1d0bb1dff4
Oli Jones <oliver.jones@diamond.ac.uk>
2026-04-29 15:11:22 +0000

    Make a script to run the full program

---
+++ src/fastcs_bacnet/core/fastcs_bacnet.py
@@ -0,0 +1,25 @@
+import asyncio
+
+from BAC0 import start
+from fastcs.launch import FastCS
+from fastcs.transports.epics import EpicsIOCOptions
+from fastcs.transports.epics.ca import EpicsCATransport
+
+from fastcs_bacnet.BAC0.bacnet_client import BacnetClient
+from fastcs_bacnet.BAC0.subscription_id import SubscriptionID
+from fastcs_bacnet.FastCS.bacnet_controller import BacnetController
+
+
+async def fastcs_bacnet(subscriptions: list[SubscriptionID]):
+
+    async with start() as bac0:
+        bacnet_client = BacnetClient(bac0, initial_subscriptions=subscriptions)
+
+        epics_ca = EpicsCATransport(epicsca=EpicsIOCOptions(pv_prefix="FASTCSBACNET"))
+        bacnet_controller = BacnetController(bacnet_client)
+
+        fastcs = FastCS(bacnet_controller, [epics_ca])
+
+        asyncio.create_task(fastcs.serve(interactive=False))
+
+        await asyncio.Event().wait()
