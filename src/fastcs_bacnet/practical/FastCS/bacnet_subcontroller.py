fe8dd9a1323152a2114066e2605249ef024806e5
Oli Jones <oliver.jones@diamond.ac.uk>
2026-05-14 10:16:14 +0000

    Move bacnet attribute classes to a new file

--- src/fastcs_bacnet/practical/FastCS/bacnet_subcontroller.py
+++ src/fastcs_bacnet/practical/FastCS/bacnet_subcontroller.py
@@ -1,134 +1,13 @@
-import asyncio
-from dataclasses import dataclass
-from typing import Any
-
-from bacpypes3.primitivedata import PropertyIdentifier
-from fastcs.attributes import AttributeIO, AttributeIORef, AttrR
+from fastcs.attributes import AttrR
 from fastcs.controllers import Controller
 from fastcs.datatypes import Float
-from fastcs.util import ONCE
 
 from fastcs_bacnet.practical.BAC0.bacnet_client import BacnetClient
 from fastcs_bacnet.practical.BAC0.subscription_id import SubscriptionID
-
-background_tasks = set()
-
-
-@dataclass
-class BacnetAttributeIORef(AttributeIORef):
-    """
-    Dataclass for referencing any subscription
-    Basically just uses subscription id dataclass
-    Update period must be once as this sets the attribute update callback
-    """
-
-    update_period: float | None = ONCE
-    subscription_id: SubscriptionID | None = None
-
-
-@dataclass
-class AnalogAttributeIORef(BacnetAttributeIORef):
-    """
-    BacnetAttributeIORef specifically for analog objects
-    """
-
-
-@dataclass
-class BinaryAttributeIORef(BacnetAttributeIORef):
-    """
-    BacnetAttributeIORef specifically for analog objects
-    """
-
-
-class BacnetAttribute:
-    """
-    Handler for bacnet attributes
-    """
-
-    """
-    bacnet_client: NOT a BAC0.lite object but a BacnetClient object
-        This is used to get ObjectSubscriptions using SubscriptionIDs
-        You cant just pass the object susbscriptions in, you need to
-        use the reference to get the object subscription from the bacnet_client
-    """
-    bacnet_client: BacnetClient
-
-    def set_actual_update(self, attr: AttrR[Any, BacnetAttributeIORef]):
-        # subscription_id should never be none
-        # finicky with default arguments
-        if attr.io_ref.subscription_id is None:
-            raise ValueError
-
-        subscription_object = self.bacnet_client.get_subscription(
-            attr.io_ref.subscription_id
-        )
-
-        if subscription_object is None:
-            print("raise error")
-            return
-
-        subscription_object.callback_holder.add(
-            lambda property_indentifier, property_value: self.actual_update(
-                attr, property_indentifier, property_value
-            )
-        )
-
-    def actual_update(
-        self,
-        attr: AttrR[Any, BacnetAttributeIORef],
-        property_indentifier: str,
-        property_value: Any,
-    ):
-        if property_indentifier == PropertyIdentifier.presentValue:
-            # could add tracking data here
-            task = asyncio.create_task(attr.update(property_value))
-            background_tasks.add(task)
-            # removes task from set when the task is done
-            task.add_done_callback(background_tasks.discard)
-
-
-class AnalogAttributeIO(AttributeIO[float, AnalogAttributeIORef], BacnetAttribute):
-    """
-    Handler for bacnet analog attributes
-    """
-
-    def __init__(self, bacnet_client: BacnetClient):
-        super().__init__()
-
-        self.bacnet_client = bacnet_client
-
-    async def update(self, attr: AttrR[float, AnalogAttributeIORef]):
-        """
-        Misnomer, does not actually update the variable in this case
-        It doesnt start the subscription either
-        It changes the callback on the subscription to update the attribute
-        (this is the actual_update procedure)
-        This is why it only needs to be run once
-        """
-
-        super().set_actual_update(attr)
-
-
-class BinaryAttributeIO(AttributeIO[float, BinaryAttributeIORef], BacnetAttribute):
-    """
-    Handler for bacnet analog attributes
-    """
-
-    def __init__(self, bacnet_client: BacnetClient):
-        super().__init__()
-
-        self.bacnet_client = bacnet_client
-
-    async def update(self, attr: AttrR[float, BinaryAttributeIORef]):
-        """
-        Misnomer, does not actually update the variable in this case
-        It doesnt start the subscription either
-        It changes the callback on the subscription to update the attribute
-        (this is the actual_update procedure)
-        This is why it only needs to be run once
-        """
-
-        super().set_actual_update(attr)
+from fastcs_bacnet.practical.FastCS.bacnet_attributes import (
+    AnalogAttributeIO,
+    AnalogAttributeIORef,
+)
 
 
 class BacnetSubController(Controller):
@@ -179,6 +58,6 @@ class BacnetSubController(Controller):
                 attribute_name,
                 AttrR(
                     Float(),
-                    io_ref=BacnetAttributeIORef(subscription_id=subscription_id),
+                    io_ref=AnalogAttributeIORef(subscription_id=subscription_id),
                 ),
             )
