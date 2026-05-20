2ecd35b61be15688ccfc756780b7efdf6618afaf
Oli Jones <oliver.jones@diamond.ac.uk>
2026-05-20 09:06:58 +0000

    Set subscriptionID and PVName

--- src/fastcs_bacnet/core/ede_parser.py
+++ src/fastcs_bacnet/core/ede_parser.py
@@ -1,9 +1,13 @@
 import csv
 
-from dls_bms.dls_bms.bms import (
+from bacpypes3.primitivedata import ObjectType
+
+from dls_bms.bms import (
     IFM_FILE_TYPE,
+    createPvName,
     extractData,
-    # getBMSConfig,
+    getBMSConfig,
+    getIniFile,
     openCsvFile,
     openExcelFile,
     skipHeader,
@@ -16,12 +20,12 @@ from fastcs_bacnet.practical.BAC0.subscription_id import (
 
 
 def parse_ede(
-    file_path: str, header_rows: int, config_path: str
+    file_path: str, header_rows: int, config_dir: str
 ) -> list[tuple[SubscriptionID, str]]:
 
     subscriptions_with_names = []
 
-    # config_file = getBMSConfig(config_path)
+    config_file = getBMSConfig(getIniFile(config_dir))
 
     if ".xls" in file_path or ".xlsx" in file_path:
         ede_file = openExcelFile(file_path)
@@ -32,17 +36,37 @@ def parse_ede(
     reader = skipHeader(reader, IFM_FILE_TYPE, headerRows=header_rows)
 
     for row in reader:
-        object_id, instance, name, existing_pv_name, data = extractData(
-            row, IFM_FILE_TYPE
+        device_instance, object_instance_str, object_name, _, optional_fields = (
+            extractData(row, IFM_FILE_TYPE)
         )
 
-        pv_name = ""  # make pv name
+        object_type_int = optional_fields[0]
+
+        ip = ip_from_row(row)
+
         subscription_id = SubscriptionID(
-            IPv4SocketAddress("", 0),
-            ObjectIdentifier("", 0),  # set subscription id
+            IPv4SocketAddress(ip, 47808),
+            ObjectIdentifier(
+                object_type_as_string(object_type_int), int(object_instance_str)
+            ),
+        )
+
+        pv_name = createPvName(
+            object_instance_str, object_name, config_file, device_instance
         )
         # do some final checks, raise exceptions, log warnings / errors
 
         subscriptions_with_names.append((subscription_id, pv_name))
 
     return subscriptions_with_names
+
+
+def ip_from_row(row: list[str]) -> str:
+    return row[17]
+
+
+def object_type_as_string(object_type: int) -> str:
+
+    object_type_enum = ObjectType(object_type)
+
+    return object_type_enum.asn1
