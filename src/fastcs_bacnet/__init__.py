fba56aac52de29985bcce924681bdc5c9491c8f2
Oli Jones <oliver.jones@diamond.ac.uk>
2026-05-22 15:40:36 +0000

    Add hacky path code for submodule
    The submodule should be one level up so we need to  add to the python
    path

--- src/fastcs_bacnet/__init__.py
+++ src/fastcs_bacnet/__init__.py
@@ -6,6 +6,16 @@
     Version number as calculated by https://github.com/pypa/setuptools_scm
 """
 
+import os
+import sys
+
 from ._version import __version__
+
+file_path = os.path.realpath(__file__)
+
+path_to_src = file_path.replace("fastcs_bacnet/__init__.py", "")
+print(path_to_src + "dls_bms")
+
+sys.path.append(path_to_src + "dls_bms")
 
 __all__ = ["__version__"]
