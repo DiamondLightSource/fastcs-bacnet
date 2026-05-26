5660c90f4e010e83666b56c0bf3ad410f3d8fcf2
Oli Jones <oliver.jones@diamond.ac.uk>
2026-05-26 10:07:27 +0000

    Replace os calls with pathlib

--- src/fastcs_bacnet/__init__.py
+++ src/fastcs_bacnet/__init__.py
@@ -6,15 +6,15 @@
     Version number as calculated by https://github.com/pypa/setuptools_scm
 """
 
-import os
+import pathlib
 import sys
 
 from ._version import __version__
 
-file_path = os.path.realpath(__file__)
+file_path = pathlib.Path(__file__)
 
-path_to_src = file_path.replace("fastcs_bacnet/__init__.py", "")
+dls_bms_path = file_path.parent.parent / "dls_bms"
 
-sys.path.append(path_to_src + "dls_bms")
+sys.path.append(str(dls_bms_path))
 
 __all__ = ["__version__"]
